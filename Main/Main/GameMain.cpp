beb56b750c7d2c046fa3d03ea0529a03220bd689
GA3 <GA3@DEV1-19>
2019-10-24 12:04:01 +0900

    画像の追加+微調整

--- Main/Main/GameMain.cpp
+++ Main/Main/GameMain.cpp
@@ -1,7 +1,7 @@
 #include "DxLib.h"
 
-#define SCREEN_PIXWIDTH		640
-#define SCREEN_PIXHEIGHT	480
+#define SCREEN_PIXWIDTH		832
+#define SCREEN_PIXHEIGHT	448
 #define POPUP_X 64
 #define POPUP_Y 64
 
@@ -20,8 +20,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 	//フォント確定
 	ChangeFontType(DX_FONTTYPE_ANTIALIASING_EDGE);
 
-
+	//先攻後攻の判定(仮置き　一旦コメントアウト中)
+	int Random[1];
+	int Green = GetColor(0, 255, 0);
 	
+	//乱数の初期値を123456に設定する
+	SRand(123456);
 
 	//ここに盤上を設定
 	//駒の種類は最大5種類
@@ -55,7 +59,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 		{ 2,3,4,5,4,3,2 },
 	};*/
 
-
+	//ランダム
+	Random[0] = GetRand(1);
 
 
 	//画像int変換関数
@@ -65,19 +70,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 	int t_chara;//仮キャラクターの表示　1体目
 	int t_chara2;//仮キャラクターの表示　2体目
 
-	 int Soldier=LoadGraph("image\\");//ここに兵士の画像
-	 int Sorcerer=LoadGraph("image\\");//ここに魔導士の画像
-	 int Espionage =LoadGraph("image\\");//ここに諜報員の画像
-	 int Knight =LoadGraph("image\\");//ここに騎士の画像
-	 int King =LoadGraph("image\\King.png");//ここに王の画像
+	int Soldier=LoadGraph("image\\");//ここに兵士の画像
+	int Sorcerer=LoadGraph("image\\");//ここに魔導士の画像
+	int Espionage =LoadGraph("image\\");//ここに諜報員の画像
+	int Knight =LoadGraph("image\\");//ここに騎士の画像
+	int King =LoadGraph("image\\King.png");//ここに王の画像
+	int EKing = LoadGraph("image\\King.png");//ここに王の画像
 
-	 //一旦ここで位置移動する。後で消すかも。
-	 int SoldX = 1, SoldY = 1;//兵士の位置X,Y
-	 int SorcX = 2, SorcY = 2;//魔導士の位置X,Y
-	 int EspiX = 3, EspiY = 3;//諜報員の位置X,Y
-	 int KnigX = 4, KnigY = 4;//騎士の位置X,Y
-	 int KingX = 5, KingY = 5;//王の位置X,Y
-	
+	//一旦ここで位置移動する。後で消すかも。
+	int SoldX = 1, SoldY = 1;//兵士の位置X,Y
+	int SorcX = 2, SorcY = 2;//魔導士の位置X,Y
+	int EspiX = 3, EspiY = 3;//諜報員の位置X,Y
+	int KnigX = 4, KnigY = 4;//騎士の位置X,Y
+	int KingX = 5, KingY = 5;//王の位置X,Y	
 	int EKingX = 6, EKingY = 6;//王の位置X,Y
 
 
@@ -95,6 +100,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 	//ゲームメインの音楽再生
 	int  Mainbgm = LoadSoundMem("sound\\");
 
+	//アニメーション登録サンプル
+	int MovieGraphHandle;
+
+	MovieGraphHandle=LoadGraph("image\\nc157304.mp4");
+	PlayMovieToGraph(MovieGraphHandle);
+
 
 
 	//test用サウンド
@@ -105,9 +116,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 
 	DrawGraph(0, 0, t_chara, TRUE);//プレイヤー1の描画
 	DrawGraph(640, 0, t_chara2, TRUE);//プレイヤー2の描画
-
-
-
+	
 	int turn=0;//ターン変数....0:自分のターン　1:相手のターン
 	int movepointX;//駒の移動の変数(MainMapと照らし合わせて使用する。)
 	
@@ -135,10 +144,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 		return -1;//エラーが出たら強制終了
 
 	}
-	
 
 
-	//
+	//while(裏画面)
+
 
 
 	//ここでゲームのメイン部分構築
@@ -152,33 +161,33 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 
 			//マウスの左クリックが押されているか
 			//マウスが押されていないとき
-			if (Mx < SCREEN_PIXWIDTH / 2)//
+			if (Mx < 192 && SCREEN_PIXHEIGHT || 640 < Mx && SCREEN_PIXHEIGHT)//
 			{
 				int x, y;
 
 				//マウスを押したときの処理
 				if ((GetMouseInputLog(&Mbutton, &Mx, &My, 1) == 0))
 				{
-					if ((GetMouseInput()&&MOUSE_INPUT_LEFT) != 0)
+					if ((GetMouseInput() && MOUSE_INPUT_LEFT) != 0)
 					{
-					//押されている
-					PlaySoundMem(se, DX_PLAYTYPE_BACK);
-					
+						//押されている
+						PlaySoundMem(se, DX_PLAYTYPE_BACK);
 
 					}
 
 
-					}
-					else
-					{
-						//押されていない
-						
+				}
+				else
+				{
+					//押されていない
 
-					}
+
+				}
 
 			}
 
-			DrawBox(0,0, SCREEN_PIXWIDTH / 2, SCREEN_PIXHEIGHT, GetColor(255, 255, 255), 1);
+			DrawBox(0, 0, 192, SCREEN_PIXHEIGHT, GetColor(0, 255, 255), 1);
+			DrawBox(640, 0, SCREEN_PIXWIDTH, SCREEN_PIXHEIGHT, GetColor(255, 0, 255), 1);
 	//選択肢の移動	
 		if (CheckHitKey(KEY_INPUT_UP))
 		{
@@ -263,8 +272,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 				}
 
 				case 4://騎士
-				{
-					DrawGraphF(x * 64 + 192, y * 64, Knight, TRUE);//騎士の画像
+				DrawGraphF(x * 64 + 192, y * 64, Knight, TRUE);//騎士の画像
+					{
 					break;
 				}
 
@@ -276,7 +285,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 
 				case 6://相手の王
 				{
-					DrawRotaGraph3(x*64+256,y*64+64,0,0, 1.0f, 1.0f, PI, EKing, TRUE);//王の画像
+					DrawRotaGraph3(x*64+256,y*64+65,0,0, 1.0f, 1.0f, PI, EKing, TRUE);//王の画像
 					break;
 				}
 
@@ -285,7 +294,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 			}
 		}
 
-		
+		//アニメーション再生サンプル
+		/*while (ProcessMessage() == 0)
+		{
+			DrawGraph(0, 0, MovieGraphHandle, FALSE);
+			WaitTimer(10);
+		}*/
 
 		//文字列表示
 		//DrawString(x, y, 表示する文字列(*char), 表示する際の文字の色(GetColor(r,g,b)));
